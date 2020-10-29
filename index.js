ed50f5dc3b72abb4fac1f384450cb0e3b9dd693a
Telma Luīze <telma@luize.com>
2020-10-29 21:57:21 +0200

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
-const app = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/plain' })
-    response.end('Hello World')
+let persons = [
+    {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+    },
+    {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+    },
+    {
+        "name": "Mo Young",
+        "number": "123-429-3958",
+        "id": 37
+    },
+    {
+        "name": "Slavoj Zizek",
+        "number": "20-5872-4728",
+        "id": 38
+    }
+]
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
 })
 
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
