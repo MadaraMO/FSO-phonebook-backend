9668b85c691f5d5ee7b2ed279ad24093ee19f778
Telma Luīze <telma@luize.com>
2020-11-01 22:36:12 +0200

    heroku

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
@@ -8,6 +9,7 @@ morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(
     ':method :url :status :res[content-length] - :response-time ms :body')
 )
+app.use(cors())
 
 // const unknownEndpoint = (req, res) => {
 //     res.status(404).send({ error: 'unknown endpoint' })
@@ -99,7 +101,7 @@ app.post('/api/persons', (req, res) => {
 
 // app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
