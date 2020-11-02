1b8aa1f27775735977615e1777d6fa551ca661e1
Telma Luīze <telma@luize.com>
2020-11-02 20:14:22 +0200

    heroku,maybe

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(
     ':method :url :status :res[content-length] - :response-time ms :body')
 )
-app.use(cors())
+
 
 // const unknownEndpoint = (req, res) => {
 //     res.status(404).send({ error: 'unknown endpoint' })
